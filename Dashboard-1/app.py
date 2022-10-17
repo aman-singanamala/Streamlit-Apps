5317e20d98dc8ed609618a9ac536f4bebfd1a780
aman-singanamala <singanamala.aman2020@vitstudent.ac.in>
2022-10-18 00:22:00 +0530

    commit

--- Dashboard-1/app.py
+++ Dashboard-1/app.py
@@ -1,8 +1,18 @@
 import streamlit as st
 
-st.header("Button")
+# import libraries
 
-if st.button("ADD"):
-    st.write("ADDITION")
-else:
-    st.write("SUBSTRACTION")
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import streamlit as st
+from datetime import datetime
+
+
+df= pd.read_csv("Aggregated_Metrics_By_Video.csv")
+
+st.write(df)
+
+# https://towardsdatascience.com/make-dataframes-interactive-in-streamlit-c3d0c4f84ccb
+
+
