0516bd1a5860c10a1fe0fe9f3966feeb3c40415e
aman-singanamala <singanamala.aman2020@vitstudent.ac.in>
2022-10-26 21:10:46 +0530

    added image

--- URL_SHORTNER/app.py
+++ URL_SHORTNER/app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import pyshorteners as ps
 
+
+
 st.header("URL Shortener")
 
-
-url= st.text_input("Enter your URL")
+st.image("i1.png")
+url= st.text_input("Enter your URL : (Example: http://example.com)", value="amansinganamala.medium.com/statistics-concepts-in-data-science-bffe06d56de6")
 
 ans= ps.Shortener().tinyurl.short(url)
 
@@ -12,3 +14,9 @@ if ans != '':
     st.write(f"Your shortened URL:{ans}")
 else:
     st.write("Enter your url")
+    
+    
+    
+    
+
+st.write("Note: Remove the link in input field and paste your URL into the Input Field")
