87526c18940feef6892862f34685347148ed9051
aman-singanamala <singanamala.aman2020@vitstudent.ac.in>
2022-10-31 21:47:06 +0530

    removed css

--- App-1/app.py
+++ App-1/app.py
@@ -3,8 +3,7 @@ import pandas as pd
 import plost
 
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
     
 st.sidebar.header("Dashboard")
 
